fac96765c0f7ec04714221c548c32656d01e15db
giantwu <giantwu@gmail.com>
2023-08-07 17:41:03 +0800

    modify git-ignore

--- env_tools/env.zsh
+++ env_tools/env.zsh
@@ -1,2 +1,4 @@
 source /usr/local/Cellar/goto/2.0.0/etc/bash_completion.d/goto.sh
 source /Users/giantwu/.gvm/scripts/gvm
+source /Users/giantwu/.nostromo/completions/nostromo.zsh
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
